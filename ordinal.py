f7d775285629ebc2211fc7be65fb5001bbbfcfa0
Tanmay Sholapurkar <165592349+innocentdevil11@users.noreply.github.com>
2024-10-24 01:37:16 +0530

    ordinal.py

--- ordinal.py
+++ ordinal.py
@@ -1,1 +1,16 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import OrdinalEncoder
+
+
+
+data = pd.DataFrame({'gender' : ['male' , 'female' , 'non binary'],
+                     'height' : ['short' , 'medium' , 'tall']})
+                     
+print(data)
+
+ordinal_encoding1 = {'short' :1 , 'medium' : 2 , 'tall' : 3 }
+data['height_encoding'] = data['height'].map(ordinal_encoding1)
+
+print(data)
 
